d9e4ee25b42bb66ccb076818abbaf6522715c108
Mohdwasim075 <161320689+Mohdwasim075@users.noreply.github.com>
2025-08-06 05:15:10 +0000

    learned on these

---
+++ Javascript/01_variables.js
@@ -0,0 +1,10 @@
+console.log('Javascript')
+
+let first_name = "Mohammed"
+
+let last_name = "wasim"
+
+console.log(first_name + " " + last_name)
+personal_email = 'wasim@google.com'  // not declaring a varialbe => not recommended 
+
+console.log("2" == 2)
