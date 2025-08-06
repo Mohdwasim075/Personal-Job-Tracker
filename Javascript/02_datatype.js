e3e37d7ea67054143bf7a856f7b818b8e310a05c
Mohdwasim075 <161320689+Mohdwasim075@users.noreply.github.com>
2025-08-06 05:35:52 +0000

    console

---
+++ Javascript/02_datatype.js
@@ -0,0 +1,9 @@
+let score =  undefined //'33'
+let score2 = '33abc'
+let scoreNumber = Number(score)
+let scoreNumber2 = Number(score2)
+
+console.log(scoreNumber)
+console.log(typeof scoreNumber);  // "33" => 33
+// console.log(scoreNumber2)
+// console.log(typeof scoreNumber2)
---
+++ Javascript/03_datatype_conversion.js
@@ -0,0 +1,15 @@
+// console.log('2' > 1)                                                   
+// console.log('02' > 1) // conversion happens before comparison
+
+// console.log("2" == 2 )  // #true ==> javascript implement conversion before comparison
+// console.log("2" === 2)  // strictly checks both the datatypes are same
+
+// console.log(null > 0)
+// console.log(null < 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+console.log(undefined > 0)
+console.log(undefined < 0)
+console.log(undefined >= 0)
+console.log(undefined ==  0)
